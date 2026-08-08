3cb05e9945fddd5c99c5910b2861ec6120664e40
Nnamdi <nnamdinumber1@gmail.com>
2026-08-08 10:49:40 +0100

    update to IT content

---
+++ javascipt/intro/script.js
@@ -0,0 +1,12 @@
+
+
+
+/* what are the difference 'let', 'var' and 'const' */
+
+
+
+//circumference of a circle 
+let len1 = Number(prompt("what is length "))
+let len2 = Number(prompt("what is breadth"))
+
+alert(len1 * len2)
