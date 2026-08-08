bc2cf79506e66531faf687fc5953bf29a33369f1
Nnamdi <nnamdinumber1@gmail.com>
2026-08-08 11:26:55 +0100

    updated conditionals

--- javascipt/conditionals/script.js
+++ javascipt/conditionals/script.js
@@ -1,13 +1,28 @@
-let age = Number(prompt("How old are you?"))
-let enterAge = 18
 
-if (age < enterAge){
-    alert("You are underage")
+// let age = Number(prompt("How old are you?"))
+// let enterAge = 18
 
-}else if(age >= enterAge){
-    alert("You are of age ")
+// if (age < enterAge){
+//     alert("You are underage")
+
+// }else if(age >= enterAge){
+//     alert("Welcome")
     
-}else{
-    alert("incorrect entry")
-}
+// }else{
+//     alert("incorrect entry")
+// }
 
+
+// let a = "8";
+// let b = 8;
+// console.log(a === b )
+
+
+/*-----------------------------------------------*/
+let cart = Number(prompt("items to check out?"))
+
+if (cart<1 || cart == 0){
+    alert("cart is empty")
+}else{
+    alert("cart is ready for check out ")
+}
