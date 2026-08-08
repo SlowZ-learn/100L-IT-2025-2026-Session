2a7bd5fc638f2fa5b8e7b8d72b52b05499c458b4
Nnamdi <nnamdinumber1@gmail.com>
2026-08-08 10:35:28 +0100

    conditonals basics

---
+++ javascipt/conditionals/script.js
@@ -0,0 +1,13 @@
+let age = Number(prompt("How old are you?"))
+let enterAge = 18
+
+if (age < enterAge){
+    alert("You are underage")
+
+}else if(age >= enterAge){
+    alert("You are of age ")
+    
+}else{
+    alert("incorrect entry")
+}
+
