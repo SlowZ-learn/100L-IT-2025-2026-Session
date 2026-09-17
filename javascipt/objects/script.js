f5c33d10c4140d87d490a44cab41978335da16cb
Nnamdi <nnamdinumber1@gmail.com>
2026-09-17 12:24:58 +0100

    feat: exercises and examples

---
+++ javascipt/objects/script.js
@@ -0,0 +1,53 @@
+// let car = {
+//     brand :"Koenigsegg",
+//     name:"Jesko",
+//     drive:function () {
+//         console.log("zooom");
+        
+//     }
+// }
+
+// console.log(car);
+// console.log(car.name);
+
+//modified the value after the property to nnamdi
+// car.name = "nnamdi";
+// console.log(car.name);
+
+// adding to the object 
+// car.chassis = "carbonfiber"
+// console.log(car);
+
+// delete an object 
+// delete car.name
+// console.log(car);
+
+//method, a function inside an object
+// car.drive()
+
+
+
+// class work 1
+// let phone = {
+//     brand : "oneplus",
+//     name: "oneplus-15t",
+//     calling: () =>{
+//         console.log("calling");
+        
+//     }
+// }
+// phone.calling()
+
+
+
+
+// this method 
+let person = {
+    name:"ivy",
+    complexion:"light skin",
+    sex:"female",
+
+    introduce : ()=>{
+        console.log(`my name is ${this.name} and i am ${this.complexion}`)
+    }
+}
